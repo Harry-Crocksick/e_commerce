a58fae32f1480e1b5e9350be4ec2c596a755004e
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2023-12-13 22:23:04 +0630

    Minor fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -35,7 +35,7 @@ export default function Navigation({ searchTerm, onSearchTerm }) {
           </button>
           <button
             type="button"
-            className="relative inline-flex justify-center items-center h-[2.875rem] w-[2.875rem] text-sm font-semibold rounded-lg border border-gray-200 bg-black text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            className="z-50 relative inline-flex justify-center items-center h-[2.875rem] w-[2.875rem] text-sm font-semibold rounded-lg border border-gray-200 bg-black text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
             data-hs-overlay="#hs-overlay-right"
           >
             <svg
